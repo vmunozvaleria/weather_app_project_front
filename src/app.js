ad76814fa0d51af0fed3fcb4e42a4243f679843b
Valentina Muñoz <vmunozvaleria@icloud.com>
2022-03-14 21:28:38 -0300

    fix name city in the front

--- src/app.js
+++ src/app.js
@@ -20,6 +20,7 @@ function displayTemperature(response) {
   console.log(response.data);
 
   let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
@@ -27,6 +28,7 @@ function displayTemperature(response) {
   let iconElement = document.querySelector("#icon");
 
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
