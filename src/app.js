62878eaa3dc661ddaa23149ffca1c7d68b4aa906
Valentina Muñoz <vmunozvaleria@icloud.com>
2022-04-09 17:21:31 -0400

    add: forecast api response URL

--- src/app.js
+++ src/app.js
@@ -24,7 +24,8 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#w_forecast");
 
   let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday"];
@@ -52,7 +53,15 @@ function displayForecast() {
   });
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
-  console.log(forecastHTML);
+  // console.log(forecastHTML);
+}
+
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "f460d367f2f35c17c3c62d368d6bd207";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  console.log(apiUrl);
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function displayTemperature(response) {
@@ -79,6 +88,8 @@ function displayTemperature(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
+
+  getForecast(response.data.coord);
 }
 
 function search(city) {
@@ -119,5 +130,3 @@ let celcius = document.querySelector("#celcius");
 celcius.addEventListener("click", displayCelciusTemperature);
 
 search("Valdivia");
-
-displayForecast();
